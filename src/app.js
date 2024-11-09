81db230a450c352ebf65e649e99126590b630446
RdeRausseo <amrausseo20@gmail.com>
2024-11-09 22:09:50 +0000

    cartas al azar generadas

--- src/app.js
+++ src/app.js
@@ -9,13 +9,56 @@ window.onload = function() {
   let pintas = ["spade", "club", "heart", "diamond"];
 
   let createCard = document.querySelector(".card");
+
   if (pintas[roll] === "spade") {
-      createCard.classList.add("spade");
-      let carta1 = document.createElement("p");
-      carta1 = 
-      let carta2 = document.createElement("p");
-      //♦ ♥ ♠ ♣
+    createCard.classList.add("spade");
+    let carta1 = document.createElement("p");
+    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
+    carta1.textContent = "♠";
+    createCard.appendChild(carta1);
+    let carta2 = document.createElement("p");
+    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
+    carta2.textContent = "♠";
+    createCard.appendChild(carta2);
+    //♦ ♥ ♠ ♣
   }
+  if (pintas[roll] === "club") {
+    createCard.classList.add("club");
+    let carta1 = document.createElement("p");
+    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
+    carta1.textContent = "♣";
+    createCard.appendChild(carta1);
+    let carta2 = document.createElement("p");
+    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
+    carta2.textContent = "♣";
+    createCard.appendChild(carta2);
+  }
+  if (pintas[roll] === "heart") {
+    createCard.classList.add("heart");
+    let carta1 = document.createElement("p");
+    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
+    carta1.textContent = "♥";
+    carta1.style.color = "red";
+    createCard.appendChild(carta1);
+    let carta2 = document.createElement("p");
+    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
+    carta2.textContent = "♥";
+    carta2.style.color = "red";
+    createCard.appendChild(carta2);
+  }
+  if (pintas[roll] === "diamond") {
+    createCard.classList.add("diamond");
+    let carta1 = document.createElement("p");
+    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
+    carta1.textContent = "♦";
+    createCard.appendChild(carta1);
+    let carta2 = document.createElement("p");
+    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
+    carta2.textContent = "♦";
+    createCard.appendChild(carta2);
+  }
+
+  //♦ ♥ ♠ ♣
   let numero = document.querySelector(".numero");
   numero.textContent = mazo[random];
 };
