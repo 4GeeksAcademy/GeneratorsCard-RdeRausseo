652d6f972465c3f43292d3fd7e9d85473be6c20c
RdeRausseo <amrausseo20@gmail.com>
2024-11-20 18:04:24 +0000

    Proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+/*Angel Rausseo */
 import "bootstrap";
 import "./style.css";
 
@@ -6,59 +7,35 @@ window.onload = function() {
   let random = Math.floor(Math.random() * 13);
   let mazo = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   let roll = Math.floor(Math.random() * 4);
-  let pintas = ["spade", "club", "heart", "diamond"];
+  let pintas = [
+    { type: "spade", figure: "♠" },
+    { type: "club", figure: "♣" },
+    { type: "heart", figure: "♥" },
+    { type: "diamond", figure: "♦" }
+  ];
 
-  let createCard = document.querySelector(".card");
+  // let pintas = ["spade", "club", "heart", "diamond"];
+  let card = document.querySelector(".card");
 
-  if (pintas[roll] === "spade") {
-    createCard.classList.add("spade");
-    let carta1 = document.createElement("p");
-    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
-    carta1.textContent = "♠";
-    createCard.appendChild(carta1);
-    let carta2 = document.createElement("p");
-    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
-    carta2.textContent = "♠";
-    createCard.appendChild(carta2);
-    //♦ ♥ ♠ ♣
-  }
-  if (pintas[roll] === "club") {
-    createCard.classList.add("club");
-    let carta1 = document.createElement("p");
-    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
-    carta1.textContent = "♣";
-    createCard.appendChild(carta1);
-    let carta2 = document.createElement("p");
-    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
-    carta2.textContent = "♣";
-    createCard.appendChild(carta2);
-  }
-  if (pintas[roll] === "heart") {
-    createCard.classList.add("heart");
-    let carta1 = document.createElement("p");
-    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
-    carta1.textContent = "♥";
-    carta1.style.color = "red";
-    createCard.appendChild(carta1);
-    let carta2 = document.createElement("p");
-    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
-    carta2.textContent = "♥";
-    carta2.style.color = "red";
-    createCard.appendChild(carta2);
-  }
-  if (pintas[roll] === "diamond") {
-    createCard.classList.add("diamond");
-    let carta1 = document.createElement("p");
-    carta1.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
-    carta1.textContent = "♦";
-    createCard.appendChild(carta1);
-    let carta2 = document.createElement("p");
-    carta2.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
-    carta2.textContent = "♦";
-    createCard.appendChild(carta2);
-  }
-
-  //♦ ♥ ♠ ♣
-  let numero = document.querySelector(".numero");
+  card.classList.add(pintas[roll].type);
+  let arriba = document.createElement("p");
+  arriba.setAttribute("class", "position-absolute top-0 start-0 mx-3 my-1");
+  arriba.style.color =
+    pintas[roll].type === "heart" || pintas[roll].type === "diamond"
+      ? "red"
+      : "black";
+  arriba.textContent = pintas[roll].figure;
+  card.appendChild(arriba);
+  let numero = document.createElement("p");
+  numero.classList.add("numero");
   numero.textContent = mazo[random];
+  card.appendChild(numero);
+  let abajo = document.createElement("p");
+  abajo.setAttribute("class", "position-absolute bottom-0 end-0 mx-3 my-1");
+  abajo.style.color =
+    pintas[roll].type === "heart" || pintas[roll].type === "diamond"
+      ? "red"
+      : "black";
+  abajo.textContent = pintas[roll].figure;
+  card.appendChild(abajo);
 };
